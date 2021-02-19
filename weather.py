c30c39065fd4b6f7efba0e53d29ee123667697d9
François Bureau <bureaufrancois@gmail.com>
2021-02-19 17:32:19 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -121,16 +121,16 @@ font_lg = ImageFont.truetype(FredokaOne, 18)
 
 datetime = time.strftime("%d/%m %H:%M")
 
-draw.text((163, 12), datetime, inky_display.BLACK, font=font_sm)
+draw.text((162, 11), datetime, inky_display.BLACK, font=font_sm)
 
-draw.text((10, 12), "Météo", inky_display.WHITE, font=font_lg)
+draw.text((12, 11), "Météo", inky_display.WHITE, font=font_lg)
 
-draw.text((10, 34), "Temperature:", inky_display.WHITE, font=font)
-draw.text((80, 34), u"{:.1f}°C".format(t_ext,1), inky_display.WHITE, font=font)
-draw.text((10, 46), "Ressentie:", inky_display.WHITE, font=font)
-draw.text((70, 46), u"{:.1f}°C".format(fl.c,1), inky_display.WHITE, font=font)
-draw.text((10, 58), "Humidite:", inky_display.WHITE, font=font)
-draw.text((70, 58), u"{:.1f}%".format(h_ext,0), inky_display.WHITE, font=font)
+draw.text((12, 34), "Temperature:", inky_display.WHITE, font=font)
+draw.text((90, 34), u"{:.1f}°C".format(t_ext,1), inky_display.WHITE, font=font)
+draw.text((12, 46), "Ressentie:", inky_display.WHITE, font=font)
+draw.text((75, 46), u"{:.1f}°C".format(fl.c,1), inky_display.WHITE, font=font)
+draw.text((12, 58), "Humidite:", inky_display.WHITE, font=font)
+draw.text((75, 58), u"{:.1f}%".format(h_ext,0), inky_display.WHITE, font=font)
 
 inky_display.set_image(img)
 inky_display.show()
