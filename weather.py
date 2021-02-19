176d9c34c78ca358506d1f7a688826a5c845928d
François Bureau <bureaufrancois@gmail.com>
2021-02-19 15:36:44 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -60,6 +60,26 @@ print('Tx humidité : ' + str(h_ext) + '%')
 print('Ressentie : ' + str(round(fl.c,1)) + '°C')
 print('Vitesse du vent : ' + str(round(ow_wind_speed,1)) + 'm/s')
 
+
+# Inkyfat functions
+
+def create_mask(source, mask=(inky_display.WHITE, inky_display.BLACK, inky_display.RED)):
+    """Create a transparency mask.
+    Takes a paletized source image and converts it into a mask
+    permitting all the colours supported by Inky pHAT (0, 1, 2)
+    or an optional list of allowed colours.
+    :param mask: Optional list of Inky pHAT colours to allow.
+    """
+    mask_image = Image.new("1", source.size)
+    w, h = source.size
+    for x in range(w):
+        for y in range(h):
+            p = source.getpixel((x, y))
+            if p in mask:
+                mask_image.putpixel((x, y), 255)
+
+    return mask_image
+
 # Display on Inkyfat
 try:
     inkyphat = auto(ask_user=True, verbose=True)
