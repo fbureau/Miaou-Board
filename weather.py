6a857511cdbd9e607099e50aada5ab47b641e9dd
François Bureau <bureaufrancois@gmail.com>
2021-02-19 14:20:52 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -73,12 +73,12 @@ if inkyphat.resolution not in ((212, 104), (250, 122)):
 inkyphat.set_border(inkyphat.BLACK)
 
 # img = Image.open(os.path.join(PATH, "resources/backdrop_miaou.png")).resize(inkyphat.resolution)
-img = Image.open("resources/backdrop_miaou.png")
+img = Image.open("resources/backdrop_miaou.png").resize(inkyphat.resolution)
 draw = ImageDraw.Draw(img)
 
-icon_kitten = Image.open("resources/icons/kitty-flower.png")
+# icon_kitten = Image.open("resources/icons/kitty-flower.png")
 # inkyphat.set_image(icon_kitten)
-draw = ImageDraw.Draw(icon_kitten)
+# draw = ImageDraw.Draw(icon_kitten)
 
 font = ImageFont.truetype(FredokaOne, 10)
 font_sm = ImageFont.truetype(FredokaOne, 6)
