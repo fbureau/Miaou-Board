ff5143eb187b8b7cf47f700e2843118041c32668
François Bureau <bureaufrancois@gmail.com>
2021-02-19 20:22:22 +0100

    Update weather.py

--- weather.py
+++ weather.py
@@ -114,7 +114,7 @@ kitty_icon = "flower"
 for icon in glob.glob("resources/icons/kitty-*.png"):
     icon_name = icon.split("kitty-")[1].replace(".png", "")
     icon_image = Image.open(icon)
-    #icon_image = icon_image.convert("RGB").quantize(palette=pal_img)
+    icon_image = icon_image.convert("RGB").quantize(palette=pal_img)
     icons[icon_name] = icon_image
     masks[icon_name] = create_mask(icon_image)
 
