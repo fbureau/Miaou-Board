09882fb2558fd72d1f36a0680965d209355532cd
François Bureau <bureaufrancois@gmail.com>
2021-03-17 17:12:38 +0100

    Update gcal.py

--- gcal.py
+++ gcal.py
@@ -10,6 +10,7 @@ from re import search
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from googleapiclient import sample_tools
 
 def main(argv):
     # Authenticate and construct service.
