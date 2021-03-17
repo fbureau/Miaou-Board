e33cc28748a07973e90ffdf56ba7622cdea2c8b8
François Bureau <bureaufrancois@gmail.com>
2021-03-17 18:16:12 +0100

    Update gcal.py

--- gcal.py
+++ gcal.py
@@ -18,19 +18,19 @@ def main(argv):
     service, flags = sample_tools.init(
         argv, 'calendar', 'v3', __doc__, __file__,
         scope='https://www.googleapis.com/auth/calendar.readonly')
-
+    
     try:
         page_token = None
         while True:
             # Call the Calendar API
             now = datetime.datetime.today().isoformat() + 'Z'
             yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).isoformat() + 'Z'   
-
+            
             events_result = service.events().list(calendarId='primary', timeMin=yesterday, timeMax=now,
                                                 maxResults=10, singleEvents=True,
                                                 orderBy='startTime').execute()
             events = events_result.get('items', [])
-
+            
             if not events:
                 print(' ')
             for event in events:
@@ -45,7 +45,7 @@ def main(argv):
                         print("C'est l'aniversaire de " + event['summary'].replace('Anniversaire ', '') )
                     else:
                         print(start.strftime('%A %d/%m') + " : " + event['summary'])
-
+                        
     except client.AccessTokenRefreshError:
         print('The credentials have been revoked or expired, please re-run'
               'the application to re-authorize.')
