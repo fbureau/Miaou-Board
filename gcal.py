972774f6a4981b1c3688cb441a3a9e623a3d2f81
François Bureau <bureaufrancois@gmail.com>
2021-03-18 16:16:57 +0100

    Update gcal.py

--- gcal.py
+++ gcal.py
@@ -18,7 +18,7 @@ try:
     creds = None
     
     if os.path.exists('client_secrets.json'):
-    creds = Credentials.from_authorized_user_file('client_secrets.json', SCOPES)
+      creds = Credentials.from_authorized_user_file('client_secrets.json', SCOPES)
     
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
