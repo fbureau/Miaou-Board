046bc0f0c962dce2b739f9784d97ce740faf6dfa
François Bureau <bureaufrancois@gmail.com>
2021-03-19 13:40:08 +0100

    Update ratp.py

--- ratp.py
+++ ratp.py
@@ -74,7 +74,6 @@ draw.text((165, 3), datetime, inky_display.BLACK, font=font_sm)
 draw.text((12, 11), "Trafic RATP", inky_display.WHITE, font=font_lg)
 draw.line((12,34, 135,34),2)
 
-text = "This could be a single line text but its too long to fit in one."
 lines = text_wrap(ratp_msg, font_sm, 130)
 line_height = 12
 
