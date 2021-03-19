8fa74a87dc36cb745981357f250568fc3c3b166c
François Bureau <bureaufrancois@gmail.com>
2021-03-19 13:26:08 +0100

    Update gcal.py

--- gcal.py
+++ gcal.py
@@ -8,57 +8,28 @@ import os.path
 import locale
 from re import search
 from oauth2client import client
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from google.oauth2 import service_account
+from googleapiclient import sample_tools
 
-try:
-    scope = "https://www.googleapis.com/auth/calendar.readonly"
-    creds = None
-    
-    if os.path.exists('client_secrets.json'):
-      creds = service_account.Credentials.from_service_account_file('client_secrets.json')
-    
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-    
-    service = build('calendar', 'v3', credentials=creds)
-    
-    while True:
-        now = datetime.datetime.today().isoformat() + 'Z'
-        yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).isoformat() + 'Z'   
+def main(argv):
+    # Authenticate and construct service.
+    service, flags = sample_tools.init(
+        argv, 'calendar', 'v3', __doc__, __file__,
+        scope='https://www.googleapis.com/auth/calendar.readonly')
 
-        events_result = service.events().list(calendarId='primary', timeMin=yesterday, timeMax=now,
-                                            maxResults=10, singleEvents=True,
-                                            orderBy='startTime').execute()
-        events = events_result.get('items', [])
-
-        if not events:
-            print(' ')
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            start = datetime.datetime.strptime(start, '%Y-%m-%d')
-            if str(start) != str((datetime.datetime.today() - datetime.timedelta(1)).strftime('%Y-%m-%d 00:00:00')):
-                if search('Poubelle', event['summary']):
-                    print("Il faut sortir la " + str.lower(event['summary']))
-                elif search('Encombrants', event['summary']):
-                    print("Les encombrants doivent passer demain")
-                elif search('Anniversaire', event['summary']):
-                    print("C'est l'aniversaire de " + event['summary'].replace('Anniversaire ', '') )
-                else:
-                    print(start.strftime('%A %d/%m') + " : " + event['summary'])
+    try:
+        page_token = None
+        while True:
+            calendar_list = service.calendarList().list(
+                pageToken=page_token).execute()
+            for calendar_list_entry in calendar_list['items']:
+                print(calendar_list_entry['summary'])
+            page_token = calendar_list.get('nextPageToken')
+            if not page_token:
                 break
 
-except client.AccessTokenRefreshError:
-    print('The credentials have been revoked or expired, please re-run'
-          'the application to re-authorize.')
+    except client.AccessTokenRefreshError:
+        print('The credentials have been revoked or expired, please re-run'
+              'the application to re-authorize.')
+
+if __name__ == '__main__':
+    main(sys.argv)
