a68be86b0a168ae02fd11ffa090a71c3cbee9349
François Bureau <bureaufrancois@gmail.com>
2021-03-23 12:33:10 +0100

    Update gcal.py

--- gcal.py
+++ gcal.py
@@ -27,7 +27,7 @@ from googleapiclient import sample_tools
 
 
 print(fa.icons['thumbs-up'])
-print(fa.format("{thumbs-up} FontAwesome is Awesome {fa-fort-awesome}"))
+print(fa.icons("{thumbs-up} FontAwesome is Awesome {fa-fort-awesome}"))
 
 def main(argv):
     with open("config/config.yaml", "r") as ymlfile:
